3f42d16c1b163970970e40a464fb8ef3783d0bf9
JatharSwati <jatharswatiofficial@gmail.com>
2023-12-24 19:40:21 +0530

    Add project files.

---
+++ P1_Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
