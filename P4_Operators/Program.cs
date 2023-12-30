65e25a9f7795331457bae7bb30d9e25ccfbead29
JatharSwati <jatharswatiofficial@gmail.com>
2023-12-30 19:00:41 +0530

    Delete operators file

---
+++ P4_Operators/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
