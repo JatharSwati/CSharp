c870cfd3471852762fc122edb4421009d768caa7
JatharSwati <jatharswatiofficial@gmail.com>
2023-12-31 06:54:55 +0530

    Implemented feedback given by pradip in User Input Exercise.

--- P3_UserInput_Exercise/Program.cs
+++ P3_UserInput_Exercise/Program.cs
@@ -9,46 +9,47 @@ namespace P3_UserInput_Exercise
     class Program
     {
         static void Main(string[] args)
+
+            //Input
         {
             string firstName, lastName, fullName, cityName, degreeName, collegeName;
             double firstYearCGPA, secondYearCGPA, thirdYearCGPA, fourthYearCGPA, aggregateCGPA;
 
-            Console.WriteLine("\nEnter your first name : ");
+            Console.Write("Enter your first name : ");
             firstName = Console.ReadLine();
 
-            Console.WriteLine("\nEnter your last name : ");
+            Console.Write("\nEnter your last name : ");
             lastName = Console.ReadLine();
 
             fullName = firstName  + " " + lastName;
 
-            Console.WriteLine("\nYour full name is : " + fullName);
-
-            Console.WriteLine("\nEnter your city name : ");
+            Console.Write("\nEnter your city name : ");
             cityName = Console.ReadLine();
 
-            Console.WriteLine("\nEnter your degree name : ");
+            Console.Write("\nEnter your degree name : ");
             degreeName = Console.ReadLine();
 
-            Console.WriteLine("\nEnter your college name : ");
+            Console.Write("\nEnter your college name : ");
             collegeName = Console.ReadLine();
 
-            Console.WriteLine("\nEnter your CGPA in each year : ");
-
-            Console.WriteLine("\nEnter your first year CGPA - ");
+            Console.Write("\nEnter your first year CGPA : ");
             firstYearCGPA = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("\nEnter your second year CGPA - ");
+            Console.Write("\nEnter your second year CGPA : ");
             secondYearCGPA = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("\nEnter your third year CGPA - ");
+            Console.Write("\nEnter your third year CGPA : ");
             thirdYearCGPA = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("\nEnter your fourth year CGPA - ");
+            Console.Write("\nEnter your fourth year CGPA : ");
             fourthYearCGPA = Convert.ToDouble(Console.ReadLine());
 
             aggregateCGPA = (firstYearCGPA + secondYearCGPA + thirdYearCGPA + fourthYearCGPA) / 4;
 
-            Console.WriteLine("\nAggregate CGPA is : " + aggregateCGPA);
+
+            // Output
+
+            Console.ForegroundColor = ConsoleColor.Green;
 
             Console.WriteLine("\n\nMy Introduction ");
             Console.Write("\nHello everyone, ");
@@ -56,11 +57,15 @@ namespace P3_UserInput_Exercise
             Console.Write("I am from " + cityName + ". ");
             Console.Write("I have completed " + degreeName + " from " + collegeName + ". ");
             Console.WriteLine("\nI have CGPA in each year as follows,");
-            Console.WriteLine("First year CGPA is " + firstYearCGPA);
-            Console.WriteLine("Second year CGPA is " + secondYearCGPA);
-            Console.WriteLine("Third Year CGPA is " + thirdYearCGPA);
-            Console.WriteLine("Fourth year CGPA is " + fourthYearCGPA);
-            Console.WriteLine("My aggregate CGPA is " + aggregateCGPA);
+            Console.WriteLine(" - First year CGPA is " + firstYearCGPA);
+            Console.WriteLine(" - Second year CGPA is " + secondYearCGPA);
+            Console.WriteLine(" - Third Year CGPA is " + thirdYearCGPA);
+            Console.WriteLine(" - Fourth year CGPA is " + fourthYearCGPA);
+            Console.WriteLine(" - My aggregate CGPA is " + aggregateCGPA);
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
