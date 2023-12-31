67ecb31c8a9e7abe3d0db556638caf667c837a77
JatharSwati <jatharswatiofficial@gmail.com>
2023-12-31 07:00:50 +0530

    Replace x and y with actual number.

--- P5_Math/Program.cs
+++ P5_Math/Program.cs
@@ -16,23 +16,25 @@ namespace P5_Math
 
             // 1. Math.Max(x,y) : 
             // The Math.Max(x,y) method can be used to find the highest value of x and y.
-            Console.WriteLine("\n1. Math.Max(x,y) : " + (Math.Max(5, 8)));
+            Console.WriteLine("\n1. Math.Max(5,8) : " + (Math.Max(5, 8)));
 
             // 2. Math.Min(x,y) :
             // The Math.Min(x,y) method can be used to find the lowest value of of x and y.
-            Console.WriteLine("\n2. Math.Min(x,y) : " + (Math.Min(4, 3)));
+            Console.WriteLine("\n2. Math.Min(4,3) : " + (Math.Min(4, 3)));
 
             // 3. Math.Sqrt(x) :
             // The Math.Sqrt(x) method returns the square root of x.
-            Console.WriteLine("\n3. Math.Sqrt(x) : " + (Math.Sqrt(25)));
+            Console.WriteLine("\n3. Math.Sqrt(25) : " + (Math.Sqrt(25)));
 
             // 4. Math.Abs(x) :
             // The Math.Abs(x) method returns the absolute (positive) value of x.
-            Console.WriteLine("\n4. Math.Abs(x) : " + (Math.Abs(-8)));
+            Console.WriteLine("\n4. Math.Abs(-8) : " + (Math.Abs(-8)));
 
             // 5. Math.Round() :
             // Math.Round() rounds a number to the nearest whole number.
-            Console.WriteLine("\n5. Math.Round() : " + (Math.Round(6.78)));
+            Console.WriteLine("\n5. Math.Round(6.78) : " + (Math.Round(6.78)));
+
+            Console.WriteLine();
         }
     }
 }
