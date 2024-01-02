f40f1f0a627152965dd7bb1be58282e4fd804904
JatharSwati <jatharswatiofficial@gmail.com>
2024-01-02 19:00:13 +0530

    Loops - Exercise - While - SumOfNumbers.

---
+++ P9_Loops_Exercise_While_SumOfNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_Loops_Exercise_While_SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Input
+
+            int i = 1, n, sum = 0;
+
+            Console.Write("Enter your number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (i<=n)
+            {
+                sum += i;       // sum = sum + i
+                i++;
+            }
+
+            // Output
+
+            Console.Write($"\nSum of 1 to {n} is {sum}");
+
+            Console.WriteLine("\n");
+
+        }
+    }
+}
