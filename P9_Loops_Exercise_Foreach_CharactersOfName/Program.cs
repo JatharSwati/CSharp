2c3b546df6ccf3fb7cffef69b3421639172e4624
JatharSwati <jatharswatiofficial@gmail.com>
2024-01-02 20:34:08 +0530

    Loops - Exercise - Foreach - CharactersOfName.

---
+++ P9_Loops_Exercise_Foreach_CharactersOfName/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_Loops_Exercise_Foreach_CharactersOfName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            string name;
+
+            Console.Write("Enter your name : ");
+            name = Console.ReadLine();
+
+            foreach (char c in name)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
