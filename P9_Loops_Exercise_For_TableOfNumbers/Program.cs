8431addc2cfa72d5408825cbeedead7ba9c8044c
JatharSwati <jatharswatiofficial@gmail.com>
2024-01-02 19:44:38 +0530

    Loops - Exercise - For - TableOfNumbers

---
+++ P9_Loops_Exercise_For_TableOfNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_Loops_Exercise_For_TableOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num;
+
+            Console.Write("Enter your number to make table : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i<= 10; i++)
+            {
+                Console.WriteLine(i * num);
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
