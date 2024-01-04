74adf5795c6428c9ca422e050b0dd013d32a2302
JatharSwati <jatharswatiofficial@gmail.com>
2024-01-04 21:51:31 +0530

    Extra Exercise - ReverseTheNumber.

---
+++ PE5_ReverseTheNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE5_ReverseTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Input 
+
+            int number, numberForOutput, reminder;
+
+            Console.Write("Enter your number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            int reverse = 0;
+            numberForOutput = number;
+
+            // Output
+
+            while (number > 0)
+            {
+                reminder = number % 10;
+                number = number / 10;
+                reverse = (reverse * 10) + reminder;                
+            }
+
+            Console.WriteLine($"\nThe reverse number of {numberForOutput} is {reverse}");
+
+            Console.WriteLine();
+
+        }
+    }
+}
