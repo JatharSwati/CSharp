70ac03d0b43840710c097792391c2ea3eb417fb1
JatharSwati <jatharswatiofficial@gmail.com>
2024-01-05 10:07:43 +0530

    Extra Exercise - PalindromeNumber.

---
+++ PE8_PalindromeNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Input
+
+            int number, reminder, sum, palindromeNumber;
+
+            Console.Write("Enter your number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            sum = 0;
+            palindromeNumber = number;
+
+
+            // Output
+
+            while (number > 0)
+            {
+                reminder = number % 10;
+                number = number / 10;
+                sum = (sum * 10) + reminder;
+            }
+
+            if (palindromeNumber == sum)
+            {
+                Console.Write($"\n{sum} is Palindrome Number.");
+            }
+            else
+            {
+                Console.WriteLine($"\n{sum} is not Palindrome Number.");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
