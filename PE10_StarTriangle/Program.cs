cc1fb00e2388fcebb6bcd1f65aa90808b13aa3ec
JatharSwati <jatharswatiofficial@gmail.com>
2024-01-06 16:03:11 +0530

    Extra Exercise - StarTriangle.

---
+++ PE10_StarTriangle/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE10_StarTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			// Input
+
+			int level;
+
+			Console.Write("Enter the level of Star Triangle : ");
+			level = Convert.ToInt32(Console.ReadLine());
+
+
+			// Output
+
+			// For level
+			for (int i = 1; i <= level; i++)
+			{
+
+				// For space
+				for (int j = 1; j <= (level - i); j++)
+				{
+					Console.Write(" ");
+				}
+
+				// For *
+				for (int k = 1; k < i * 2; k++)
+				{
+					Console.Write("*");
+				}
+
+				Console.WriteLine();
+			}
+
+            Console.WriteLine();
+		}
+    }
+}
